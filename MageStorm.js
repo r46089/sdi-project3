f105972d5180e229f1ac2014ab2f6ace8d2e7e4c
r46089 <Omnifarious@fullsail.edu>
2013-06-20 19:09:03 -0700

    Retooling, story blocks, good stuff.

--- MageStorm.js
+++ MageStorm.js
@@ -12,19 +12,54 @@ var Monster =
 }
 
 var monsters = [];
-var monsterNames = ["Flynn", "Quinn", "Lynn", "Gwyn", "Mike"];
+var battles = 0;
 
-function spawnMonsters() {
+function spawnMonsters(count) {
 	var i = 0;
 	var newMonsters = [];
 	var thisMonster = Monster;
-	for (i = 0; i < monsterNames.length; i++) {
-		thisMonster.setName(monsterNames[i]);
+	for (i = 0; i < mageStormSettings.monsterNames.length && monsterNames.length > 0; i++) {
+		thisMonster.setName(monsterNames.pop());
 		newMonsters.push(thisMonster);
 		thisMonster = Monster;
 	}
 
 	return newMonsters;
+}
+
+console.log("The mage creeped around the demon's den, cautious not to attract attention.");
+console.log("Unfortunately for our hero, in this dungeon, the creatures see all...");
+
+while (monsters.length) {
+	if (battles > 0) {
+		console.log("But the fight wasn't over yet, and a new wave appeared...");
+	}
+
+	var i = 0;
+	var lastMonster = "";
+	for (i = 0; i < monsters.length; i++) {
+		if (i) lastMonster = monsters[i-1].getName();
+		if (!(i % 2)) {
+			console.log("\"My name is " + monsters[i].getName() + ",\" the beast began.");
+			console.log("But it was given no leave to finish its statement, before it was");
+			console.log("hit by a fireball for 5 damage!");
+			monsters[i].strike(5); 
+		} else {
+			console.log("\"" + lastMonster + "! I will heal you!\" And it was done by " + monsters[i].getName());
+			console.log("before he was struck with a fireball for 5 damage.");
+			monsters[i].strike(5);
+			monsters[i-1].heal(3);
+		}
+		console.log("(to those who know like you and me, this left poor " + monsters[i].getName());
+		if (!(i % 2)) {
+			console.log("with " + monsters[i].getHP() + " health)");
+		} else {
+			console.log("with " + monsters[i].getHP() + " health -- but " + lastMonster + " now had ");
+			console.log(monsters[i-1].getHP() + " health!)");
+		}
+	}
+
+	if (!monsters.length) monsters = spawnMonsters(++battles);
 }
 
 
