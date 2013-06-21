2dec3aaf11ad8eb03fd7f2260403773044735257
r46089 <Omnifarious@fullsail.edu>
2013-06-20 18:39:53 -0700

    Organization, and a function to spawn new monsters.

--- MageStorm.js
+++ MageStorm.js
@@ -1,14 +1,30 @@
 
 var Monster =
 {
-  .HP: 15,
-  .getHP: function() { return this.HP; }
-  .strike: function (damage) { if (this.damage > this.HP) this.HP = 0; else this.HP = this.HP - damage; }
-  .heal: function (heal) { this.HP = this.HP + heal; }
   .name: "";
-  .getName: function() { return this.name; }
-  .setName: function(newName) { this.name = newName; }
+  .HP: 15,
+  .getHP: function() { return this.HP; },
+  .strike: function (damage) { if (this.damage > this.HP) this.HP = 0; else this.HP = this.HP - damage; },
+  .heal: function (heal) { this.HP = this.HP + heal; },
+  .getName: function() { return this.name; },
+  .setName: function(newName) { this.name = newName; },
   .isAlive: function() { if (this.HP <= 0) return false; else return true; }
+}
+
+var monsters = [];
+var monsterNames = ["Flynn", "Quinn", "Lynn", "Gwyn", "Mike"];
+
+function spawnMonsters() {
+	var i = 0;
+	var newMonsters = [];
+	var thisMonster = Monster;
+	for (i = 0; i < monsterNames.length; i++) {
+		thisMonster.setName(monsterNames[i]);
+		newMonsters.push(thisMonster);
+		thisMonster = Monster;
+	}
+
+	return newMonsters;
 }
 
 
